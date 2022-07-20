2d65a784c339136efc1c4efabdb1a81e1e5cafa2
Elagoht <furkanbaytekin@gmail.com>
2022-07-21 00:05:59 +0300

    Site Reborn

--- behaviour.js
+++ behaviour.js
@@ -1,9 +1,57 @@
-const menuswitch=document.getElementById("menuswitch")
-const aside=document.getElementById("sidemenu")
-let toggle=false
+const projects=document.getElementById('projects')
+const figures=document.getElementsByClassName("fig-media")
+const figure_prevs=document.getElementsByClassName("fig-prev")
+const figure_nexts=document.getElementsByClassName("fig-next")
+function gallery_prev() {
+  projects.scrollBy({ 
+    top: 0, // could be negative value
+    left: -430, 
+    behavior: 'smooth' 
+  });
+}
+function gallery_next() {
+  projects.scrollBy({ 
+    top: 0, // could be negative value
+    left: 430, 
+    behavior: 'smooth' 
+  });
+}
 
-function menu() {
-    aside.style.width=toggle?"4rem":"16rem"
-    toggle=!toggle
-    console.log(toggle)
+let figslides=[]
+for (let i=0;i<figures.length;++i) {
+  figslides[i]=0
+}
+let figslidesmax=[]
+for (let i=0;i<figures.length;++i) {
+  figslidesmax[i]=figures[i].getElementsByTagName("*").length
+}
+
+function fig_next(fignum) {
+  medias=figures[fignum].getElementsByTagName("*")
+  for (let i=0;i<medias.length;++i) {
+    medias[i].style.display="none"
+  }
+  if (figslidesmax[fignum]-1>figslides[fignum]) {
+    figslides[fignum]+=1
+  }
+  else {
+    figslides[fignum]=0
+  }
+  medias[figslides[fignum]].style.display="block"
+}
+function fig_prev(fignum) {
+  medias=figures[fignum].getElementsByTagName("*")
+  for (let i=0;i<medias.length;++i) {
+    medias[i].style.display="none"
+  }
+  if (figslides[fignum]>0) {
+    figslides[fignum]-=1
+  } else {
+    figslides[fignum]=figslidesmax[fignum]-1
+  }
+  medias[figslides[fignum]].style.display="block"
+}
+for (let i=0;i<figslides.length;++i) {
+  fig_prev(i)
+  fig_next(i) 
 }
