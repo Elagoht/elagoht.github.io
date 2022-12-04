b25c5ed4fe4871a4149e1c0a5a2fad131dbc15c1
Furkan Baytekin <furkanbaytekin@gmail.com>
2022-12-04 23:45:34 +0300

    style: background & feat: image loading

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from "framer-motion"
 function Certificate({ data }) {
 
   const [popUp, setPopUp] = useState(false)
+  const [load, setLoad] = useState(false)
 
   return <div className="card">
     <div className="flex justify-between w-full gap-4">
@@ -35,7 +36,23 @@ function Certificate({ data }) {
           id="close-menu"
           className="flex relative items-center justify-center"
         >
-          <img src={data.document} alt={data.title} className="max-w-full max-h-full" />
+          <img src={data.document} alt={data.title} className="max-w-full max-h-full" onLoad={() => setLoad(true)} />
+          {!load && <motion.div
+            initial={{ scale: 3 }}
+            animate={{ rotate: [0, 360], transition: { duration: 2, repeat: Infinity, ease: "anticipate" } }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" className="fill-current text-zinc-50">
+              <circle cx="13.792" cy="3.082" r="3.082" />
+              <circle cx="13.792" cy="24.501" r="1.849" />
+              <circle cx="6.219" cy="6.218" r="2.774" />
+              <circle cx="21.365" cy="21.363" r="1.541" />
+              <circle cx="3.082" cy="13.792" r="2.465" />
+              <circle cx="24.501" cy="13.791" r="1.232" />
+              <path d="M4.694,19.84c-0.843,0.843-0.843,2.207,0,3.05c0.842,0.843,2.208,0.843,3.05,0c0.843-0.843,0.843-2.207,0-3.05
+			C6.902,18.996,5.537,18.988,4.694,19.84z"/>
+              <circle cx="21.364" cy="6.218" r="0.924" />
+            </svg>
+          </motion.div>}
         </motion.div>
       </div>
       }
--- src/components/Skeleton/Profile.js
+++ src/components/Skeleton/Profile.js
@@ -9,7 +9,7 @@ function Profile() {
   const { language } = useContext(LanguageContext)
 
   return <aside id="profile">
-    <div className="bg-code-snippet bg-fixed max-md:py-8 py-24 bg-top">
+    <div className="bg-code-snippet bg-fixed max-md:py-8 py-24 bg-top" style={{ backgroundPosition: "top 3.5rem" }}>
       <div className="flex max-md:flex-col justify-around items-center transition-all max-w-screen-lg m-auto gap-4 max-lg:mx-8">
         <div className="transition-all border-2 rounded-full w-64 max-lg:w-52 aspect-square bg-profile-picture bg-contain shrink-0"></div>
         <div className="flex flex-col text-center justify-center gap-2">
